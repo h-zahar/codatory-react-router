816e7f5d9d4642f3c355098c31607b26b6773117
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-10-16 08:20:17 +0600

    Built 404 Not Found Page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,9 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h4>404 Not Found</h4>
+            <div style={{height: '50vh'}} className="d-flex justify-content-center align-items-center">
+            <h4 className="text-center text-danger">404 Not Found</h4>
+            </div>
         </div>
     )
 }
